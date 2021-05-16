02010e0341a69a636a847fcda8c8cff9c2356222
dozkni <dozkni@gmail.com>
2021-05-17 00:07:17 +0600

    rest

--- api/core/views.py
+++ api/core/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
 
-# Create your views here.
+from rest_framework import viewsets
+from .serializers import PostSerializer
+from .models import Post
+#from rest_framework.response import Response
+from rest_framework import permissions
+
+
+class PostViewSet(viewsets.ModelViewSet):
+    serializer_class = PostSerializer
+    queryset = Post.objects.all()
+    lookup_field = 'slug'
+    permission_classes = [permissions.AllowAny]
